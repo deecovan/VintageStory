e6d90fbe18795a154ecf6e678d4f011bb8076cb6
Dee <dee.covan@gmail.com>
2025-01-26 20:46:31 +0200

    Start new DeeCovan Rivers 70-200-30-200 10k/5k/256 200rare 3daysNoDrop

---
+++ run1280.bat
@@ -0,0 +1,4 @@
+C:\Programs\QRes.exe /x:1280 /y:720
+copy clientsettings.json.fix.1280x720 clientsettings.json
+"C:\Games\VintageStory\PlayVintageStory.exe"
+copy clientsettings.json clientsettings.json.fix.1280x720
---
+++ run1366.bat
@@ -0,0 +1,4 @@
+C:\Programs\QRes.exe /x:1366 /y:768
+copy clientsettings.json.fix.1366x768 clientsettings.json
+"C:\Games\VintageStory\PlayVintageStory.exe"
+copy clientsettings.json clientsettings.json.fix.1366x768
