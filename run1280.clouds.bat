f45057f458aad0bba43d19612590dbdaab95966f
Dee <dee.covan@gmail.com>
2026-02-08 13:16:49 +0200

    Up: q1366 settings added

--- run1280.clouds.bat
+++ run1280.clouds.bat
@@ -1,4 +1,4 @@
 C:\Programs\QRes.exe /x:1280 /y:720
-copy clientsettings.json.fix.1280x720.clouds clientsettings.json
+copy clientsettings.json.fix.1280.clouds clientsettings.json
 "C:\Games\VintageStory\PlayVintageStory.exe"
-copy clientsettings.json clientsettings.json.fix.1280x720.clouds
+copy clientsettings.json clientsettings.json.fix.1280.clouds
